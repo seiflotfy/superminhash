c081c42a3e21c0ef9890cdeb5a639c626f3241fc
Seif Lotfy <seif.lotfy@gmail.com>
2018-01-01 18:04:45 +0100

    the signature value updates r + j are strictly increasing within the inner loop.  erefore, if we knew the current maximum of all current signature values, we would be able to leave the inner loop early

--- superminhash.go
+++ superminhash.go
@@ -14,8 +14,8 @@ type Signature struct {
 	p      []uint16
 	q      []int64
 	b      []int64
-	a      int64
 	i      int64
+	a      uint16
 }
 
 // NewSignature ...
@@ -38,8 +38,9 @@ func NewSignature(length uint16) (*Signature, error) {
 		values: values,
 		p:      p,
 		q:      q,
+		b:      b,
 		i:      0,
-		a:      int64(length) - 1,
+		a:      uint16(length) - 1,
 	}, nil
 }
 
@@ -55,9 +56,9 @@ func (sig *Signature) Push(b []byte) {
 	d := metro.Hash64(b, 42)
 	rnd := pcgr.New(int64(d), 0)
 
-	for j := range sig.values {
+	for j := uint16(0); j < sig.a; j++ {
 		r := float64(rnd.Next()) / float64(math.MaxUint32)
-		offset := rnd.Next() % uint32(len(sig.values)-j)
+		offset := rnd.Next() % uint32(uint16(len(sig.values))-j)
 		k := uint32(j) + offset
 
 		if sig.q[j] != sig.i {
@@ -72,7 +73,17 @@ func (sig *Signature) Push(b []byte) {
 
 		sig.p[j], sig.p[k] = sig.p[k], sig.p[j]
 		rj := r + float64(j)
-		sig.values[sig.p[j]] = math.Min(sig.values[sig.p[j]], float64(rj))
+		if rj < sig.values[sig.p[j]] {
+			jc := uint16(math.Min(sig.values[sig.p[j]], float64(len(sig.values)-1)))
+			sig.values[sig.p[j]] = rj
+			if j < jc {
+				sig.b[jc]--
+				sig.b[j]++
+				for sig.b[sig.a] == 0 {
+					sig.a--
+				}
+			}
+		}
 	}
 	sig.i++
 }
@@ -84,7 +95,6 @@ func (sig *Signature) Similarity(other *Signature) (float64, error) {
 		return 0, fmt.Errorf("signatures not of same length, sign has length %d, while other has length %d", len(sig.values), len(other.values))
 	}
 	for i, element := range sig.values {
-		// fmt.Println(element, other.values[i], element == other.values[i])
 		if element == other.values[i] {
 			sim++
 		}
